f057cbbf18ab01b85cb99d121b9b25af4be852c9
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-07 08:22:21 +0800

    245. working with nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,17 @@
+import { Route } from 'react-router-dom';
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      {/* <Route path='new-user'> Relative path */}
+      {/* <Route path='/test/new-user'> This will not render */}
+      {/* Absolute path */}
+      <Route path='/welcome/new-user'>
+        <p>Welcome, new user!</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
